00fb9f5013f15e2e30c5e3adddba9fc5aa19e8c1
Michael Lopez <mlopez@securityscorecard.io>
2017-11-14 08:03:49 -0500

    Added support for & and | on IPv4 addresses.

---
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/expressions/netExpressions.scala
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.catalyst.expressions
+
+import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode}
+import org.apache.spark.sql.types.{DataType, Ipv4AddressType}
+
+// * `ipv4_bit_or(a,a): a`
+// * `ipv4_bit_and(a,a): a`
+// * `ipv4_distance(a,a): L`
+// * `ipv4_forward(a,L): a`
+// * `ipv4_compliment(a): a`
+// * `ipv4_eq(a,a): B`
+// * `ipv4_neq(a,a): B`
+// * `ipv4_less(a,a): B`
+// * `ipv4_less_eq(a,a): B`
+// * `ipv4_greater(a,a): B`
+// * `ipv4_greater_eq(a,a): B`
+// * `ipv4_from_string(s): a`
+// * `ipv4_from_binary(b): a`
+
+case class IPv4BitOr(left: Expression, right: Expression)
+  extends BinaryExpression with ExpectsInputTypes {
+
+  override def inputTypes: Seq[DataType] = Seq(Ipv4AddressType, Ipv4AddressType)
+
+  override def dataType: DataType = Ipv4AddressType
+
+  protected override def nullSafeEval(x: Any, y: Any): Any = {
+    x.asInstanceOf[Ipv4AddressType.InternalType] | y.asInstanceOf[Ipv4AddressType.InternalType]
+  }
+
+  override protected def doGenCode(
+    ctx: CodegenContext,
+    ev: ExprCode
+  ): ExprCode = {
+    nullSafeCodeGen(ctx, ev, (x, y) => {
+      s"""
+        ${ev.value} = $x | $y;
+        ${ev.isNull} = ${ev.value} == null;
+       """
+    })
+  }
+}
+
+case class IPv4BitAnd(left: Expression, right: Expression)
+  extends BinaryExpression with ExpectsInputTypes {
+
+  override def inputTypes: Seq[DataType] = Seq(Ipv4AddressType, Ipv4AddressType)
+
+  override def dataType: DataType = Ipv4AddressType
+
+  protected override def nullSafeEval(x: Any, y: Any): Any = {
+    x.asInstanceOf[Ipv4AddressType.InternalType] & y.asInstanceOf[Ipv4AddressType.InternalType]
+  }
+
+  override protected def doGenCode(
+    ctx: CodegenContext,
+    ev: ExprCode
+  ): ExprCode = {
+    nullSafeCodeGen(ctx, ev, (x, y) => {
+      s"""
+        ${ev.value} = $x & $y;
+        ${ev.isNull} = ${ev.value} == null;
+       """
+    })
+  }
+}
